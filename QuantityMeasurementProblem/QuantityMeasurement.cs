69c6950bbce9abf5678d16d47a659de84ce1e693
vishal-patil01 <vishalpatil01@hotmail.com>
2020-08-14 12:17:32 +0530

    [Vishal] Initial Commit : Created QuantityMeasurement Project

---
+++ QuantityMeasurementProblem/QuantityMeasurement.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="QuantityMeasurement.cs" company="BridgeLabz">
+// Copyright (c) Vishal Rajput. All rights reserved.
+// </copyright>
+namespace QuantityMeasurementProblem
+{
+    using System;
+
+    /// <summary>
+    /// Converting Quantity From one Unit To Another Unit.
+    /// </summary>
+    public class QuantityMeasurement
+    {
+    }
+}
---
+++ QuantityMeasurementProblemTest/QuantityMeasurementTest.cs
@@ -0,0 +1,21 @@
+// <copyright file="QuantityMeasurementTest.cs" company="BridgeLabz">
+// Copyright (c) Vishal Rajput. All rights reserved.
+// </copyright>
+namespace QuantityMeasurementProblemTest
+{
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Class to Test QuantityMeasurement.
+    /// </summary>
+    public class QuantityMeasurementTest
+    {
+        /// <summary>
+        /// Setup Method.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+        }
+    }
+}
